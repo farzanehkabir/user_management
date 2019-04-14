2ad67ce559b3f08dee0a6a464092bf99a81fea35
farzaneh <farzanehkabirbeyk@gmail.com>
2019-04-15 00:48:44 +0430

    به قول استاد make some changes

--- ConsoleApp2/AllDate.cs
+++ ConsoleApp2/AllDate.cs
@@ -24,10 +24,11 @@ namespace ConsoleApp2
 		}
 		public static DateTime ConverShamsiToMiladi(string time)
 		{
+			System.Globalization.PersianCalendar persianCalender = new System.Globalization.PersianCalendar();
 			int year = Convert.ToInt32(time.Substring(0, 4));
 			int month = Convert.ToInt32(time.Substring(5, 2));
 			int day = Convert.ToInt32(time.Substring(8, 2));
-			DateTime miladiDate = new DateTime(year, month, day);
+			DateTime miladiDate = new DateTime(year, month, day,persianCalender);
 
 
 			return miladiDate;
@@ -45,15 +46,17 @@ namespace ConsoleApp2
 			return hijriDate;
 		}
 
+		
+
 		public static DateTime ConverGhamariToMiladi(string time)
 		{
-
+			System.Globalization.HijriCalendar hijriCalendar = new System.Globalization.HijriCalendar();
 			int year = Convert.ToInt32(time.Substring(0, 4));
 			int month = Convert.ToInt32(time.Substring(5, 2));
 			int day = Convert.ToInt32(time.Substring(8, 2));
-			DateTime miladiDate = new DateTime(year, month, day);
-			return miladiDate;
+			DateTime miladiDate = new DateTime(year, month, day,hijriCalendar);
 
+			return miladiDate;
 		}
 
 		public static Date ConverMiladiToHebrew(System.DateTime time)
@@ -65,6 +68,17 @@ namespace ConsoleApp2
 			int day = hebrewCalendar.GetDayOfMonth(time);
 			Date hebrewDate = new Date(year: year, month: month, day: day);
 			return hebrewDate;
+		}
+
+		public static DateTime ConverHebrewToMiladi(string time)
+		{
+			System.Globalization.HebrewCalendar hebrewCalendar = new System.Globalization.HebrewCalendar();
+			int year = Convert.ToInt32(time.Substring(0, 4));
+			int month = Convert.ToInt32(time.Substring(5, 2));
+			int day = Convert.ToInt32(time.Substring(8, 2));
+			DateTime miladiDate = new DateTime(year, month, day, hebrewCalendar);
+
+			return miladiDate;
 		}
 
 
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,29 +10,51 @@ namespace ConsoleApp2
 	{
 		static void Main(string[] args)
 		{
-			Date persianDate = AllDate.ConvertMiladToShamsi(System.DateTime.Now);
-			Console.WriteLine(persianDate.GetString());
-			System.Console.WriteLine(persianDate.Year);
-			System.Console.WriteLine(persianDate.Month);
-			System.Console.WriteLine(persianDate.Day);
+
+			System.DateTime dateNow = System.DateTime.Now;
+			
+			System.Console.WriteLine(dateNow.ToString("yyyy/MM/dd hh:mm:ss"));
+			System.Console.WriteLine(dateNow);
+			System.Console.WriteLine("--------------------------------------------");
+
+			Date persianDate = AllDate.ConvertMiladToShamsi(dateNow);
+			Console.WriteLine($"ConvertMiladToShamsi : \t\t {persianDate.GetString()}");
+			//System.Console.WriteLine(persianDate.Year);
+			//System.Console.WriteLine(persianDate.Month);
+			//System.Console.WriteLine(persianDate.Day);
 			System.Console.WriteLine();
 
-			Date hijriDate = AllDate.ConverMiladiToGhamari(System.DateTime.Now);
-			Console.WriteLine(hijriDate.GetString());
-			System.Console.WriteLine(hijriDate.Year);
-			System.Console.WriteLine(hijriDate.Month);
-			System.Console.WriteLine(hijriDate.Day);
+			Date hijriDate = AllDate.ConverMiladiToGhamari(dateNow);
+			Console.WriteLine($"ConverMiladiToGhamari : \t {hijriDate.GetString()}");
+			//System.Console.WriteLine(hijriDate.Year);
+			//System.Console.WriteLine(hijriDate.Month);
+			//System.Console.WriteLine(hijriDate.Day);
 			System.Console.WriteLine();
 
-			Date hebrewDate = AllDate.ConverMiladiToHebrew(System.DateTime.Now);
-			Console.WriteLine(hebrewDate.GetString());
-			System.Console.WriteLine(hebrewDate.Year);
-			System.Console.WriteLine(hebrewDate.Month);
-			System.Console.WriteLine(hebrewDate.Day);
+			Date hebrewDate = AllDate.ConverMiladiToHebrew(dateNow);
+			Console.WriteLine($"ConverMiladiToHebrew : \t\t {hebrewDate.GetString()}");
+			//System.Console.WriteLine(hebrewDate.Year);
+			//System.Console.WriteLine(hebrewDate.Month);
+			//System.Console.WriteLine(hebrewDate.Day);
 			System.Console.WriteLine();
 
+			string persianDate1 = persianDate.GetString();
+			DateTime miladiDate = AllDate.ConverShamsiToMiladi(persianDate1);
+			//System.Console.WriteLine(miladiDate.Year);
+			//System.Console.WriteLine(miladiDate.Month);
+			//System.Console.WriteLine(miladiDate.Day);
 
+			Console.WriteLine($"ConverShamsiToMiladi : \t\t {miladiDate.Year}/{miladiDate.Month}/{miladiDate.Day}");
+			Console.WriteLine();
 
+			string hijriDate1 = hijriDate.GetString();
+			DateTime miladiDate1 = AllDate.ConverGhamariToMiladi(hijriDate1);
+			//Console.WriteLine(miladiDate1.Year);
+			//Console.WriteLine(miladiDate1.Month);
+			//Console.WriteLine(miladiDate1.Day);
+
+			Console.WriteLine($"ConverGhamariToMiladi : \t {miladiDate1.Year}/{miladiDate1.Month}/{miladiDate1.Day}");
+			System.Console.WriteLine();
 
 
 
